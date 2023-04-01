c692f1407186d43532046a76a4ca8aca9db7137e
Yernar Ayanuly <erjan.ernar2002@gmail.com>
2023-04-01 11:59:59 +0600

    fix/authwt jwt config

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
 
 from api import router
 from core import configs, get_db
+from fastapi_jwt_auth import AuthJWT
+from fastapi_jwt_auth.exceptions import AuthJWTException, JWTDecodeError
+from fastapi.responses import JSONResponse, RedirectResponse
+
 
 app = FastAPI(
     title=configs.PROJECT_NAME,
@@ -11,3 +15,16 @@ app = FastAPI(
 )
 
 app.include_router(router)
+
+
+@AuthJWT.load_config
+def get_config():
+    return configs
+
+
+@app.exception_handler(AuthJWTException)
+def authjwt_exception_handler(request: Request, exc: AuthJWTException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.message}
+    )
